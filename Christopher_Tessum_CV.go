8520dbce70df999a12af1b97d72b0ba8ec5ed2b4
Christopher Tessum <ctessum@gmail.com>
2024-09-17 16:10:00 -0500

    Move Xiaokai Paper

--- Christopher_Tessum_CV.go
+++ Christopher_Tessum_CV.go
@@ -71,6 +71,7 @@ var cv = []Section{
 	{
 		Name: "Peer-Reviewed Publications <small>(*=corresponding author; self and advisees are underlined)</small>",
 		Citations: []template.HTML{
+			"yang2024atmospheric",
 			"giang2024", "Peshin_2024", "Schollaert_2024", "Schollaert2023", "ywang2023", "Nawaz2023",
 			"gallagher2023", "jackson2023city", "thind2022environmental",
 			"yuzhou2022ej", "kleiman2022", "mtessum2022",
@@ -85,7 +86,7 @@ var cv = []Section{
 	{
 		Name: "Preprints and Manuscripts Submitted for Review <small>(*=corresponding author; self and advisees are underlined)</small>",
 		Citations: []template.HTML{
-			"yang2024atmospheric", "park2023learned", "KelpNN2018",
+			"park2023learned", "KelpNN2018",
 		},
 	},
 	// {
